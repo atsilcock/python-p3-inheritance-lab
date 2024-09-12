a6889fc101ab6a6551854bf37b8341d0cc83af78
Alin Silcock <at.silcock@gmail.com>
2024-09-11 22:37:52 -0600

    update

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,6 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+       
+    def learn(self, lesson=""):
+        self.knowledge.append(lesson)
--- lib/teacher.py
+++ lib/teacher.py
@@ -2,9 +2,14 @@
 
 from user import User
 
-import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Initial knowledge"]
 
-    def teach(self):
-        pass
+    def teach(self, learned_information=None):
+        if learned_information is None:
+            learned_information = "Deault Knowledge"
+        self.knowledge.append(learned_information)
+        return learned_information
+        
